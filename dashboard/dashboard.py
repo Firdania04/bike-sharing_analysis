b7e9f8a10c976df797bf4b34e4dc4404eee058bf
Firdania04 <firdania3103@gmail.com>
2024-10-07 00:38:12 +0800

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -44,7 +44,7 @@ def create_rfm_df(df):
     return rfm_df
 
 # Prepare dataframe
-day_df = pd.read_csv("day_clean.csv")
+day_df = pd.read_csv("dashboard/day_clean.csv")
 
 # Ensure the date column are of type datetime
 datetime_columns = ["date"]
